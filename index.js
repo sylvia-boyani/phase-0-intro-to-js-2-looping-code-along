97dc1354348332accd4550318c2c7e035fb7dd5a
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-05 00:42:04 +0300

    commit tree 9e6bd537331a232befca4c33307927a2aa549de1

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-// const gifts = ["teddy bear", "drone", "doll"];
+// // const gifts = ["teddy bear", "drone", "doll"];
 
-// function wrapGifts(gifts) {
-//   for (let i = 0; i < gifts.length; i++) {
-//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     debugger;
-//   }
+// // function wrapGifts(gifts) {
+// //   for (let i = 0; i < gifts.length; i++) {
+// //     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+// //     debugger;
+// //   }
 
-//   return gifts;
+// //   return gifts;
+// // }
+
+// // wrapGifts(gifts);
+
+
+// //const cards = ["charlie", "samip", "ali"];
+
+// function writeCards(cards) {
+//     for (let i = 0; i > cards.length; i++) {
+//         console.log( `Thank you, ${cards[i]} for the wonderful gift!`);
+//     }
+//     return cards;
 // }
+// writeCards(cards);
 
-// wrapGifts(gifts);
+// // let favoriteNumber = 0;
+// // while (favoriteNumber < 10) {
+// //   favoriteNumber = favoriteNumber + 1;
+// // }
+// // console.log(favoriteNumber);
 
-
-const cards = ["charlie", "samip", "ali"];
-
-function writeCards(cards) {
-    for (let i = 0; i > cards.length; i++) {
-        console.log( `Thank you, ${cards[i]} for the wonderful gift!`);
-        debugger;
+const newArray=[];
+function writeCards(stringArray,eventName){
+    for (let i=0; i < stringArray.length; i++)
+    {
+// newArray.push(stringArray[i]);
+newArray.push(`Thank you ${stringArray[i]}, for the wonderful ${eventName}gift!`);
+debugger
     }
-    return cards;
+    return newArray;
 }
-writeCards(cards);
-
-// let favoriteNumber = 0;
-// while (favoriteNumber < 10) {
-//   favoriteNumber = favoriteNumber + 1;
-// }
-// console.log(favoriteNumber);
