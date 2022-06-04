c675cfc9e2cb44cba5cc11151d488cf17e351fe1
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-05 00:48:15 +0300

    done

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-// // const gifts = ["teddy bear", "drone", "doll"];
+// const gifts = ["teddy bear", "drone", "doll"];
 
-// // function wrapGifts(gifts) {
-// //   for (let i = 0; i < gifts.length; i++) {
-// //     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-// //     debugger;
-// //   }
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
 
-// //   return gifts;
-// // }
-
-// // wrapGifts(gifts);
-
-
-// //const cards = ["charlie", "samip", "ali"];
-
-// function writeCards(cards) {
-//     for (let i = 0; i > cards.length; i++) {
-//         console.log( `Thank you, ${cards[i]} for the wonderful gift!`);
-//     }
-//     return cards;
+//   return gifts;
 // }
-// writeCards(cards);
 
-// // let favoriteNumber = 0;
-// // while (favoriteNumber < 10) {
-// //   favoriteNumber = favoriteNumber + 1;
-// // }
-// // console.log(favoriteNumber);
+// wrapGifts(gifts);
+
+
+Const cards = ["charlie", "samip", "ali"];
+
+function writeCards(cards) {
+    for (let i = 0; i > cards.length; i++) {
+        console.log( `Thank you, ${cards[i]} for the wonderful gift!`);
+    }
+    return cards;
+}
+writeCards(cards);
+
+//WHILE LOOP
+
+let favoriteNumber = 0;
+while (favoriteNumber < 10) {
+  favoriteNumber = favoriteNumber + 1;
+}
+console.log(favoriteNumber);
 
 const newArray=[];
 function writeCards(stringArray,eventName){
