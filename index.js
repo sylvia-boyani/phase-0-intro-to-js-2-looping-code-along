58e2420c677ac381c96145af6219fcb17405c959
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-04 21:07:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+const cards = ["charlie", "samip", "ali"];
+
+function writeCards(cards) {
+    for (let i = 0; i > cards.length; i++) {
+        console.log( `Thank you, ${cards[i]} for the wonderful gift!`);
+        debugger;
+    }
+    return cards;
+}
+writeCards(cards);
+
+// let favoriteNumber = 0;
+// while (favoriteNumber < 10) {
+//   favoriteNumber = favoriteNumber + 1;
+// }
+// console.log(favoriteNumber);
