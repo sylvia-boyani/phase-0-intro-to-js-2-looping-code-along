5b8eee7b8fc58b00ef1558f0fd0bf1ce6c2b54f4
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-10 16:45:36 +0300

    commit tree 4b6a23fd054f165ec90e05c15d0fde73542c8d13

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-// const gifts = ["teddy bear", "drone", "doll"];
-
-// function wrapGifts(gifts) {
-//   for (let i = 0; i < gifts.length; i++) {
-//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//     debugger;
-//   }
-
-//   return gifts;
-// }
-
-// wrapGifts(gifts);
-
-
-Const cards = ["charlie", "samip", "ali"];
-
-function writeCards(cards) {
-    for (let i = 0; i > cards.length; i++) {
-        console.log( `Thank you, ${cards[i]} for the wonderful gift!`);
-    }
-    return cards;
+const writeCards = (names) => {
+  return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
 }
-writeCards(cards);
 
-//WHILE LOOP
-
-let favoriteNumber = 0;
-while (favoriteNumber < 10) {
-  favoriteNumber = favoriteNumber + 1;
-}
-console.log(favoriteNumber);
-
-const newArray=[];
-function writeCards(stringArray,eventName){
-    for (let i=0; i < stringArray.length; i++)
-    {
-// newArray.push(stringArray[i]);
-newArray.push(`Thank you ${stringArray[i]}, for the wonderful ${eventName}gift!`);
-debugger
-    }
-    return newArray;
-}
+const countDown = (number) => {
+  for (let i = number; i >= 0; i--) {
+      console.log(i)
+  }
+} 
